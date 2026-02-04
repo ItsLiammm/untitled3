ddf811aae6b6f4bea87a3309cafed74a82657115
liam <liam.renzini@stud.unifi.it>
2026-02-04 11:36:29 +0100

    consegna 5.3

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/liam/CLionProjects/untitled3
+# Build directory: /Users/liam/CLionProjects/untitled3/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/liam/CLionProjects/untitled3/cmake-build-debug/runTests[1]_include.cmake")
+subdirs("_deps/googletest-build")
